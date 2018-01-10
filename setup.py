a9d51227c914c33b83f14dc4e3e28358119ad3e4
hmallen <allenhm@gmail.com>
2018-01-10 17:59:49 -0500

    Modified setup.py.

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+try:
+    with open('DESCRIPTION.rst', 'r') as f:
+        longDesc = f.read()
+except:
+    print('Warning: Could not open DESCRIPTION.rst.  long_description will be set to None.')
+    longDesc = None
 
 setup(
     name = "drawSVG",
+    packages = find_packages(),
     version = "1.0.0",
+    description = ("A fork of Peter Collingridge's DrawSVG that works with Python 3."),
     author = "Hunter Allen",
     author_email = "allenhm@gmail.com",
-    description = ("A fork of Peter Collingridge's DrawSVG that works with Python 3."),
-    packages=['drawSVG'],
 )
